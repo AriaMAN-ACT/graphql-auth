7578dba72170d84656373998d7a60fdaca68945c
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-11 11:36:06 +0430

    create signout mutation

--- server/schema/mutation.js
+++ server/schema/mutation.js
@@ -24,6 +24,14 @@ const mutation = new GraphQLObjectType({
             resolve(parentValue, {email, password}, req) {
                 return AuthService.signup({email, password, req});
             }
+        },
+        signout: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                const {user} = req;
+                req.logout();
+                return user;
+            }
         }
     }
 });

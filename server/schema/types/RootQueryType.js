f708eac0e5936f83a121095308d1855e8e891882
WolfMAN <aria.azadi.pour@protonmail.com>
2020-06-11 11:40:14 +0430

    get current user from root query

--- server/schema/types/RootQueryType.js
+++ server/schema/types/RootQueryType.js
@@ -1,9 +1,19 @@
 const graphql = require('graphql');
+
+const UserType = require('./UserType');
+
 const { GraphQLObjectType } = graphql;
 
 const RootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
-    fields: {}
+    fields: {
+        user: {
+            type: UserType,
+            resolve(parentValue, args, req) {
+                return req.user;
+            }
+        }
+    }
 });
 
 module.exports = RootQueryType;
